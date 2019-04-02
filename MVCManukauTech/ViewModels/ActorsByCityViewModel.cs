aaa3e003d446fc0551b9a195bb9d65c7767a2c83
JiawenYe <827038228@qq.com>
2019-04-02 14:26:55 +1300

    project start

---
+++ MVCManukauTech/ViewModels/ActorsByCityViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCManukauTech.ViewModels
+{
+    public class ActorsByCityViewModel
+    {
+        [Key]
+        public string QueryKey { get; set; }
+        public string City { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Relationship { get; set; }
+    }
+}
